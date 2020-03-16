76d97fa1acc9faaf9fe34d1df714861efb0b9174
sanjelarun <sanjelarun@gmail.com>
2020-03-16 13:06:15 -0500

    New Data Structure, Expression not added

--- ast/ast_test.py
+++ ast/ast_test.py
@@ -1,12 +1,23 @@
 import ast
-import multiprocessing as mp;
 
 
 class ForLoop:
 
     def __init__(self, initial_line_number, final_line_number):
+        self.allVariables = []
         self.initial_line_number = initial_line_number
         self.final_line_number = final_line_number
+        self.operations = []
+        self.conditions = []
+
+    def add_conditions(self, condition):
+        self.conditions.append(condition)
+
+    def add_operations(self, operation):
+        self.operations.append(operation)
+
+    def add_variables(self, variable):
+        self.allVariables.append(variable)
 
 
 allForLoops = []
@@ -14,10 +25,17 @@ with open("../examples/sum.py") as fin:
     tree = ast.parse(fin.read())
 for x in ast.walk(tree):
     if isinstance(x, ast.For):
-        print(x.lineno)
-        print(x.body[-1].lineno)
-        allForLoops.append(ForLoop(x.lineno, x.body[-1].lineno))
+        temp_f = ForLoop(x.lineno, x.body[-1].lineno)
+        for node in ast.walk(x):
+            if isinstance(node, ast.Name) and isinstance(node.ctx, ast.Store):
+                if not temp_f.allVariables.__contains__(node.id):
+                    temp_f.add_variables(node.id)
+            if isinstance(node, ast.Expr):
+                print(node)
+                temp_f.add_operations(node.expr)
+        allForLoops.append(temp_f)
 print('All loops')
 for a in allForLoops:
-
     print(a.initial_line_number, a.final_line_number)
+    print(a.allVariables)
+    print(a.operations)
