036780a0aa82abe5f5837c50e070ffbc7c5ef9cb
sanjelarun <sanjelarun@gmail.com>
2020-03-17 14:42:25 -0500

    Programa and Function Information Added

--- ast/ast_test.py
+++ ast/ast_test.py
@@ -1,7 +1,24 @@
 import ast
 
 
-class ForLoop:
+## Store Program information
+class ProgramInformation:
+    def __init__(self):
+        self.functions: []
+
+
+## Store all function information
+class FunctionInformation:
+
+    def __init__(self):
+        self.name = []
+        self.iteration = []
+        self.input_variable = []
+        self.return_type = ""
+
+
+## Store all loop information [Right now it is only for loop]
+class LoopInformation:
 
     def __init__(self, initial_line_number, final_line_number):
         self.allVariables = []
@@ -20,22 +37,46 @@ class ForLoop:
         self.allVariables.append(variable)
 
 
-allForLoops = []
-with open("../examples/sum.py") as fin:
-    tree = ast.parse(fin.read())
-for x in ast.walk(tree):
-    if isinstance(x, ast.For):
-        temp_f = ForLoop(x.lineno, x.body[-1].lineno)
-        for node in ast.walk(x):
-            if isinstance(node, ast.Name) and isinstance(node.ctx, ast.Store):
-                if not temp_f.allVariables.__contains__(node.id):
-                    temp_f.add_variables(node.id)
-            if isinstance(node, ast.Expr):
-                print(node)
-                temp_f.add_operations(node.expr)
-        allForLoops.append(temp_f)
-print('All loops')
-for a in allForLoops:
-    print(a.initial_line_number, a.final_line_number)
-    print(a.allVariables)
-    print(a.operations)
+def extracted_loops(node):
+    ## Extraction Phase
+    allForLoops = []
+    for x in ast.walk(node):
+        if isinstance(x, ast.For):
+            temp_f = LoopInformation(x.lineno, x.body[-1].lineno)
+            for node in ast.walk(x):
+               ## print(node)
+                if isinstance(node, ast.Name) and isinstance(node.ctx, ast.Store):
+                    if not temp_f.allVariables.__contains__(node.id):
+                        temp_f.add_variables(node.id)
+                if isinstance(node, ast.Add):
+                    print(node)
+                    ## temp_f.add_operations(node.operator)
+            allForLoops.append(temp_f)
+    print('All loops')
+    for a in allForLoops:
+        print(a.initial_line_number, a.final_line_number)
+        print(a.allVariables)
+        print(a.operations)
+
+
+def funtion_analysis(node):
+    for x in ast.walk(node):
+        extracted_loops(x)
+
+
+def program_analysis():
+    with open("../examples/sum.py") as fin:
+        tree = ast.parse(fin.read())
+    for x in ast.walk(tree):
+        print(x)
+        if isinstance(x, ast.FunctionDef):
+            print("here")
+            funtion_analysis(x)
+
+
+def main():
+    program_analysis()
+
+
+if __name__ == "__main__":
+    main()
--- examples/sum.py
+++ examples/sum.py
@@ -8,6 +8,10 @@ def sum_array(numbers):
         sum_a -= n
     return sum_all
 
+## For AST information
+def no_loop():
+    print("Hello")
+
 
 numbers = [1, 2, 3, 4, 5, 5, 10, 12]
 print(sum_array(numbers))
